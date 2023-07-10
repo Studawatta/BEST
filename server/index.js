899fc01659ee43fd9aaa55526534e390b51819b8
Studawatta <st.udawatte@gmail.com>
2023-07-10 23:22:08 +0530

    user controllers updated

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config()
+const connectDB = require("./config/db")
 const port = process.env.PORT || 5000
 
+connectDB()
+
 const app = express()
+app.use(express.json())
+
+app.use('/api/users',userRoute)
 
 app.listen(port,()=>console.log(`Server started on port ${port}`))
